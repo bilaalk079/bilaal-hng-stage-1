64aea62ee9e4363240b11f26093eac6fde9fa8d7
bilaalk079 <kolawolebilaal13@gmail.com>
2025-10-22 11:34:56 +0100

    added a / endpoint

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({
+      message:
+        "Welcome to Bilaal's string analyzer, visit https://github.com/bilaalk079/bilaal-hng-stage-1 to view the README / Docs",
+    });
+});
 app.use("/strings", stringRoutes);
 
 // MongoDB connection
